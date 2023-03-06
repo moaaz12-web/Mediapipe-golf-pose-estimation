b8524779b8b0700e98d724e7250206ef560fc953
moaaz12-web <moaaz7151@gmail.com>
2023-03-06 23:48:42 +0500

    A bug fix

--- Readme.md
+++ Readme.md
@@ -4,10 +4,13 @@
 
 ### To run,
 
-<code>python app.py --path_to_video_1 --path_to_video_2 base_directory_for_results --type_of_video1 --type_of_video2</code>
+<code>python app.py path_to_video_1 path_to_video_2 base_directory_for_results type_of_video1 type_of_video2 frameword_lengh_seconds</code>
 
 
 <ul>
-  <li> <code>type_of_video1</code> and <code>type_of_video2</code>  are used to describe whether the video is a user uploaded video or a base (professional) video. In case of base, pass in 1, otherwise pass a 0. 
+  <li> <code>type_of_video1</code> and <code>type_of_video2</code>  are used to describe whether the video is a user uploaded video or a base (professional) video. In case of base, pass in 1, otherwise pass a 0.  
+  
+  
+  The <code>frameword_lengh_seconds</code> specifies the length for each image split, example, 1 sec, 0.5 sec, 0.25 sec etc
   </li>
  </ul>
--- app.py
+++ app.py
@@ -3,29 +3,40 @@ import math
 import pandas as pd
 from compute import compute
 from writeDataframe import writeDataframe
+import sys
 
 
-def main(video1_path, video2_path, base_directory_path, video1_type, video2_type):
+def main(video1_path, video2_path, base_directory_path, video1_type, video2_type, framework_length_seconds):
 
-    dataframe1 = compute(video1_path, base_directory_path, video1_type)
-    dataframe2 = compute(video2_path, base_directory_path, video2_type)
+    dataframe1 = compute(video1_path, base_directory_path, video1_type, framework_length_seconds)
+    dataframe2 = compute(video2_path, base_directory_path, video2_type, framework_length_seconds)
 
     writeDataframe(dataframe1, dataframe2, base_directory_path)
 
-    
-    # return dataframe1, dataframe2
+
+if __name__ == "__main__":
+    if len(sys.argv) != 7:
+        print("Usage: python main.py <video1_path> <video2_path> <base_directory_path> <video1_type> <video2_type> <framework_length_seconds>")
+        sys.exit(1)
+
+    video1_path = sys.argv[1]
+    video2_path = sys.argv[2]
+    base_directory_path = sys.argv[3]
+    video1_type = int(sys.argv[4])
+    video2_type = int(sys.argv[5])
+    framework_length_seconds = float(sys.argv[6])
+
+    main(video1_path, video2_path, base_directory_path, video1_type, video2_type, framework_length_seconds)
 
 
-main(r'C:\Users\moaaz\3D Objects\vid2.mp4',r'C:\Users\moaaz\3D Objects\vid3.mp4', 'D:/finale_last', 1, 0)
 
-
-# df1, df2 = main(r'C:\Users\moaaz\3D Objects\vid2.mp4',r'C:\Users\moaaz\3D Objects\vid3.mp4', 'D:/finale_last', 1, 0)
-                        # |                                  # |
-                                                             # |
-                        # |
-                # video1_path,                             video2_path
+# df1, df2 = main(r'C:\Users\moaaz\3D Objects\vid2.mp4',r'C:\Users\moaaz\3D Objects\vid3.mp4', 'D:/finale_last', 1, 0, 3)
+                        # |                                  # |                                                         |
+                                                             # |                                                         |
+                        # |                                  # |                                                         |
+                # video1_path                             video2_path                                              framework_length_seconds
 
                                               
-                                                # 0 for user uploaded video
+                                                # 0 for user uploaded video                                         Specifies the length for each image split, example, 1 sec, 0.5 sec, 0.25 sec etc
                                                 # 1 for etalon video
                                                                 
